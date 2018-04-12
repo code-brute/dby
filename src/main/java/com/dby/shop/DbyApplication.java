274da238eff1e127899652bcd98f82beb63caa63
jackson <m18292930341@163.com>
2018-04-12 12:47:00 +0800

    配置数据源

--- src/main/java/com/dby/shop/DbyApplication.java
+++ src/main/java/com/dby/shop/DbyApplication.java
@@ -1,12 +1,26 @@
 package com.dby.shop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class DbyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DbyApplication.class, args);
+	static Logger logger = LoggerFactory.getLogger(DbyApplication.class);
+	public static void main(String[] args) throws UnknownHostException {
+		SpringApplication app = new SpringApplication(DbyApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		logger.info("Access URLs:\n----------------------------------------------------------\n\t" +
+						"Local: \t\thttp://127.0.0.1:{}\n\t" +
+						"External: \thttp://{}:{}\n----------------------------------------------------------",
+				env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"));
 	}
 }
---
+++ src/main/java/com/dby/shop/config/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @Description:
+ * @author: mitnick
+ * @date: 2018-04-12 下午12:14
+ */
+package com.dby.shop.config;
