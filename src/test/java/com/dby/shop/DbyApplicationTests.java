0c10067f1beeb1b4dee2ef8df39f110a9912aab2
jackson <m18292930341@163.com>
2018-04-12 11:45:23 +0800

    搭建环境

---
+++ src/test/java/com/dby/shop/DbyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dby.shop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
